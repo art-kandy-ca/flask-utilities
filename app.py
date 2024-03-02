a23e6259b6d4133b9ac564f540686816c82f27cc
Artem Kandinskyi <artem.kandinskyi@gmail.com>
2024-03-02 15:36:36 +0200

    Implemented Frame Timecode Converter

--- app.py
+++ app.py
@@ -1,81 +1,53 @@
 import sqlite3
-from flask import Flask, render_template, request, url_for, flash, redirect
-from werkzeug.exceptions import abort
-
-def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
-    return conn
-
-def get_post(post_id):
-    conn = get_db_connection()
-    post = conn.execute('SELECT * FROM posts WHERE id = ?',
-                        (post_id,)).fetchone()
-    conn.close()
-    if post is None:
-        abort(404)
-    return post
+from flask import Flask, render_template, request, url_for, flash
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'fksifjhajfoiwehfijvksdfilsnadfkjsdahijfvhnavli'
 
 @app.route('/')
 def index():
-    conn = get_db_connection()
-    posts = conn.execute('SELECT * FROM posts').fetchall()
-    conn.close()
-    return render_template('index.html', posts=posts)
+    return render_template('index.html')
 
-@app.route('/<int:post_id>')
-def post(post_id):
-    post = get_post(post_id)
-    return render_template('post.html', post=post)
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/frames_to_timecodes', methods=('GET', 'POST'))
+def frames_to_timecodes():
+	timecodes = ""
+	if request.method == 'POST':		
+		try:
+			fps = float(request.form['fps'])
+		except ValueError:
+			fps = 0
+			flash('FPS value is invalid!')
+		
+		if fps > 0:
+			frames = request.form['frames']	
+			frames = ''.join(frames)
+			frames = frames.splitlines()
 	
-@app.route('/create', methods=('GET', 'POST'))
-def create():
-    if request.method == 'POST':
-        title = request.form['title']
-        content = request.form['content']
-
-        if not title:
-            flash('Title is required!')
-        else:
-            conn = get_db_connection()
-            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
-                         (title, content))
-            conn.commit()
-            conn.close()
-            return redirect(url_for('index'))
+			if len(frames) > 0 and frames[0] != "0":
+				frames.insert(0, 0)
+	
+			chapter = 1
+			for frame in frames:
+				try:
+					frame = int(frame)
+					sec_float = frame/fps
+				except ValueError:
+					flash("Frame {} value is invalid!".format(chapter))
+					timecodes = ""
+					break
 			
-    return render_template('create.html')
-	
-@app.route('/<int:id>/edit', methods=('GET', 'POST'))
-def edit(id):
-    post = get_post(id)
+				h = int(sec_float/60/60)
+				sec_float -= h * 60 * 60
+				m = int(sec_float/60)
+				sec_float -= m * 60
+				s = int(sec_float)
+				sec_float -= s
+				ms = int(sec_float * 1000)
+				timecodes += "CHAPTER{chapter:02d}={h:02d}:{m:02d}:{s:02d}.{ms:03d}\r\nCHAPTER{chapter:02d}NAME=Chapter {chapter:d}\r\n".format(chapter=chapter,h=h,m=m,s=s,ms=ms)
+				chapter+=1
 
-    if request.method == 'POST':
-        title = request.form['title']
-        content = request.form['content']
-
-        if not title:
-            flash('Title is required!')
-        else:
-            conn = get_db_connection()
-            conn.execute('UPDATE posts SET title = ?, content = ?'
-                         ' WHERE id = ?',
-                         (title, content, id))
-            conn.commit()
-            conn.close()
-            return redirect(url_for('index'))
-
-    return render_template('edit.html', post=post)
-	
-@app.route('/<int:id>/delete', methods=('POST',))
-def delete(id):
-    post = get_post(id)
-    conn = get_db_connection()
-    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
-    conn.commit()
-    conn.close()
-    flash('"{}" was successfully deleted!'.format(post['title']))
-    return redirect(url_for('index'))
+	return render_template('frames_to_timecodes.html', timecodes=timecodes)
---
+++ app2.py
@@ -0,0 +1,152 @@
+import sqlite3
+from flask import Flask, render_template, request, url_for, flash, redirect
+from werkzeug.exceptions import abort
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'fksifjhajfoiwehfijvksdfilsnadfkjsdahijfvhnavli'
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
+
+@app.route('/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+    return render_template('post.html', post=post)
+	
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                         (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+			
+    return render_template('create.html')
+	
+@app.route('/<int:id>/edit', methods=('GET', 'POST'))
+def edit(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE posts SET title = ?, content = ?'
+                         ' WHERE id = ?',
+                         (title, content, id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('edit.html', post=post)
+	
+@app.route('/<int:id>/delete', methods=('POST',))
+def delete(id):
+    post = get_post(id)
+    conn = get_db_connection()
+    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+    flash('"{}" was successfully deleted!'.format(post['title']))
+    return redirect(url_for('index'))
+	
+@app.route('/frames_to_timecodes', methods=('GET', 'POST'))
+def frames_to_timecodes():
+	title = None
+	if request.method == 'POST':
+		title = request.form['title']
+	
+	return render_template('frames_to_timecodes.html', timecodes=title)
+	
+@app.route('/query-example')
+def query_example():
+    # if key doesn't exist, returns None
+    language = request.args.get('language')
+
+    # if key doesn't exist, returns a 400, bad request error
+    framework = request.args.get('framework')
+
+    # if key doesn't exist, returns None
+    website = request.args.get('website')
+
+    return '''
+              <h1>The language value is: {}</h1>
+              <h1>The framework value is: {}</h1>
+              <h1>The website value is: {}'''.format(language, framework, website)
+
+# allow both GET and POST requests
+@app.route('/form-example', methods=['GET', 'POST'])
+def form_example():
+	# handle the POST request
+	language=None
+	framework=None
+	if request.method == 'POST':
+		language = request.form.get('language')
+		framework = request.form.get('framework')
+	
+	# otherwise handle the GET request
+	return '''
+           <form method="POST">
+               <div><label>Language: <input type="text" name="language"></label></div>
+               <div><label>Framework: <input type="text" name="framework"></label></div>
+               <input type="submit" value="Submit">
+           </form>
+		   <h1>The language value is: {}</h1>
+           <h1>The framework value is: {}</h1>'''.format(language, framework)
+
+# GET requests will be blocked
+@app.route('/json-example', methods=['POST'])
+def json_example():
+    request_data = request.get_json()
+
+    language = request_data['language']
+    framework = request_data['framework']
+
+    # two keys are needed because of the nested object
+    python_version = request_data['version_info']['python']
+
+    # an index is needed because of the array
+    example = request_data['examples'][0]
+
+    boolean_test = request_data['boolean_test']
+
+    return '''
+           The language value is: {}
+           The framework value is: {}
+           The Python version is: {}
+           The item at index 0 in the example list is: {}
+           The boolean value is: {}'''.format(language, framework, python_version, example, boolean_test)
+
+if __name__ == '__main__':
+    # run app in debug mode on port 5000
+    app.run(debug=True, port=5000)
